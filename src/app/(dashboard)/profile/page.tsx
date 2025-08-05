898c6d56efa5b47a7a1818a1cb11be6357653751
codethelabs <codethelabs@gmail.com>
2025-08-05 21:53:52 +0300

    more changes

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 "use client";
-
+// force 
 import { useState } from 'react';
 import { useAuth } from '@/app/lib/AuthContext';
 import PrivateRoute from '@/app/components/PrivateRoute';
